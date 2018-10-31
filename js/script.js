b06b493adeba930ed2d296006680a55602778d6d
Zac Lucarelli <chikanz@outlook.com>
2018-10-31 22:38:48 +1100

    Finito :tada:
    css reset
    stroke everything
    gradient Bg + font
    anims
    game over wav
    etc.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@
 var daddy = document.querySelector('.scene');
 var bigdaddy = document.querySelector('#tridiv');
 
+var ins = document.querySelector(".instruction");
+
 bigdaddy.addEventListener("click", checkTarget);
 bigdaddy.addEventListener("click", gameStart)
 bigdaddy.addEventListener("mousemove", rotate);
 
-//BGM
+//BGM + loop when ended (https://stackoverflow.com/questions/3273552/html5-audio-looping)
 var BGM = new Audio('sound/Loopster.mp3');
+BGM.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
 
 //Rotate vars
 var angleX = 0;
@@ -53,12 +59,12 @@ function rotate(event) {
 }
 
 
-var tolerance = 30 / 2;
+var tolerance = 50 / 2;
 class target {
-    constructor(name, selector, X, Y,) 
+    constructor(name, selector, X, Y, Xstart, YStart, XAnim, YAnim) 
     {
-        this.name = name;
-        this.selector = selector;
+        this.name = name; //name for sound clips + images
+        this.selector = selector; //face selector
 
         //Calc bounds from tolerance
         this.xmin = X - tolerance;
@@ -67,23 +73,39 @@ class target {
         this.ymax = Y + tolerance;
 
         this.audio = new Audio('sound/' + name + '.wav'); //init audio
+
+        this.root = selector.split(" ")[0]; //root shape        
+
+        //For the push anims
+        this.Xstart = Xstart;
+        this.YStart = YStart;
+        this.XAnim = XAnim;
+        this.YAnim = YAnim;
     }
 }
 
 var titles = 
 [
-    'bork it',
-    'hit it', 
-    'doot it',
-    'play it',
+    'Bork it!',
+    'Hit it!', 
+    'Doot it!',
+    'Play it!',
+]
+
+var colors = 
+[
+    'fc9c7c',
+    '7cd9fc',
+    'ecd34a',
+    '66e870',
 ]
 
 var targets =
 [
-    new target("bork",".cub-2 .rt", 90, -360),  //Red 
-    new target("oof",".cub-3 .lt", -90, -360),  //Blue
-    new target("doot",".cub-4 .tp", 0, -450),   //Yellow
-    new target("bop",".cub-4 .bm", 0, -270,),   //Green
+    new target("bork",".cub-2 .rt", 90, -360, -8, 0, -7, 0),  //Red 
+    new target("oof",".cub-3 .lt", -90, -360,  8, 0, 7, 0),  //Blue
+    new target("doot",".cub-4 .tp", 0, -450,   0, 0, 0, 1),   //Yellow
+    new target("bop",".cub-4 .bm", 0, -270,    0, 0, 0, -1),   //Green
 ]
 
 function checkTarget(event) 
@@ -108,17 +130,30 @@ function checkTarget(event)
         currentTarget = -1;
 
         //Turn off instruction
-        document.querySelector(".instruction").innerHTML = "";
+        ins.innerHTML = "";
+        ins.classList.remove('shadowLine');
 
         //Sound
         t.audio.play();
 
-        //Set face image to smasked and back again
+        //anim
+        document.querySelector(t.selector.split(" ")[0]).setAttribute("style",getAnimString(t.XAnim,t.YAnim)) //aaaa
+
+        setTimeout(() => {
+            document.querySelector(t.selector.split(" ")[0]).setAttribute("style",getAnimString(t.Xstart,t.YStart));
+        }, 350);
+
+        //Set face image to smasked and 1back again
         smack(t, true);
         setTimeout(() => {
            smack(t, false) 
         }, 1000);
     }
+}
+
+function getAnimString(x, y)
+{
+    return "transform:translate3D(" + x + "em, " + y + "em, 0em); transition: 0.5s";
 }
 
 function smack(target, on)
@@ -134,11 +169,14 @@ function newTarget()
 
     currentTarget = getRandomInt(targets.length);
 
-    //call out sound here
+    //call out sound here 
+    ins.innerHTML = titles[currentTarget];
+    ins.classList.add('shadowLine');
 
-    document.querySelector(".instruction").innerHTML = titles[currentTarget];
+    var col = score < 7 ? colors[currentTarget] : 'd3d3d3'
+    ins.setAttribute("style", "background: #" + col + ";");
 
-    if(TimeToBop > 1) TimeToBop -= timerDecrease;
+    if(TimeToBop > 1000) TimeToBop -= timerDecrease;
     timer = TimeToBop;
 }
 
@@ -148,7 +186,7 @@ function getRandomInt(max) {
 
 function gameStart()
 {
-    if(runGame) return;
+    if(runGame || !canclick) return;
 
     BGM.play();
     runGame = true;
@@ -168,11 +206,14 @@ function gameStart()
     bar.set(1);
 }
 
+var gameover = new Audio('sound/gameover.wav');
+var canclick = true;
 function gameOver()
 {
     runGame = false;
-
-    document.querySelector(".instruction").innerHTML = "Game over!";
+    canclick = false;
+    gameover.play();
+    document.querySelector(".instruction").innerHTML = "Game over! <br> Click to play again";
 
     var hs = window.localStorage.getItem('score')
     if(score > hs)
@@ -180,6 +221,13 @@ function gameOver()
         window.localStorage.setItem('score',score);
         document.querySelector(".highscore").innerHTML = hs;
     }
+
+    ins.setAttribute("style", "background: #959595;");
+
+    //So we don't click through game over
+    setTimeout(() => {
+        canclick = true;
+    }, 1000);
     
 }
 
